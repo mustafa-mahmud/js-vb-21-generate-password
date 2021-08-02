151b5bf066b9f2e4f0826c71decc846d57af5d8f
mustafa-mahmud <mithuweb000@gmail.com>
2021-08-02 15:55:41 +0600

    end project

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const i = document.querySelector('i');
+const i = document.querySelector('.fa-eye-slash');
 const input = document.querySelector('input');
 const copy = document.querySelector('.fa-copy');
 const generate = document.querySelector('.fa-sync-alt');
 const spanCopy = document.querySelector('.copy-it');
+const cross = document.querySelector('.fa-times');
 
 const passLetters = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%';
 
 function toggoleEye() {
+  console.log(456);
   this.classList.toggle('fa-eye-slash');
   this.classList.toggle('fa-eye');
 
@@ -36,7 +38,13 @@ function copyPass() {
   }
 }
 
+function delPassword() {
+  input.value = '';
+  input.placeholder = 'Your password...';
+}
+
 ///////////////
 i.addEventListener('click', toggoleEye);
 generate.addEventListener('click', generatePass);
 copy.addEventListener('click', copyPass);
+cross.addEventListener('click', delPassword);
